777a4dd479147624748484672626b6edda09480b
Viet Quang <93778662+helloitsurdvq@users.noreply.github.com>
2023-11-07 23:08:05 +0700

    Update C06

--- 20215235_DinhVietQuang_C06.asm
+++ 20215235_DinhVietQuang_C06.asm
@@ -1,11 +1,11 @@
 .data
-	cnt: .word 256
-	s: .space 21
-	inputMessage: .asciiz "Input string: "
-	outputMessage: .asciiz "Result: "
-	appearance: .asciiz "Appearance: "
-	space: .asciiz " "
-	newline: .asciiz "\n"
+cnt: .word 256
+s:	.space 21
+inputMessage: .asciiz "Input string: "
+outputMessage: .asciiz "Result: "
+appearance: .asciiz "Appearance: "
+space: .asciiz " "
+newline: .asciiz "\n"
 .text
 # $s0, $s1: i, j of loop
 init:
@@ -35,29 +35,34 @@ end_of_string:
 	move $t0, $s0 # $t0: size of string
 	
 	li $s0, 97
-	li $s1, 97 # $s1 = maxChar
+	li $s1, 97 # $s1 = minChar
 	la $t3, cnt
 	mul $v0, $s0, 4
 	add $t3, $t3, $v0 # $t3 = addr(cnt['A'])
-	lw $s2, ($t3) # $s2 = cnt[maxChar]
+	lw $s2, ($t3) # $s2 = cnt[minChar]
 	li $s0, 98
 	
-check_max_loop:
-	beq $s0, 123, after_check_max_loop
+check_min_loop:
+	beq $s0, 123, after_check_min_loop
 	la $t3, cnt
 	mul $v0, $s0, 4
 	add $t3, $t3, $v0 # $t3 = addr(cnt[i])
 	lw $t4, ($t3) # $t4 = cnt[i]
-	ble $t4, $s2, not_change
 	
+	beq $t4, $zero, not_change
+	beq $s2, $zero, adjust_min_char
+	
+	bgt $t4, $s2, not_change
+	
+adjust_min_char:
 	move $s1, $s0
 	move $s2, $t4
 	
 not_change:
 	addi $s0, $s0, 1
-	j check_max_loop
+	j check_min_loop
 	
-after_check_max_loop:
+after_check_min_loop:
 	li $v0, 4
 	la $a0, outputMessage
 	syscall
@@ -75,7 +80,7 @@ after_check_max_loop:
 	syscall
 	
 	li $s0, 0
-find_max_in_string:
+find_min_in_string:
 	beq $s0, $t0, end_main
 	la $t3, s
 	add $t3, $t3, $s0 # $t3 = addr(s[i])
@@ -92,7 +97,7 @@ find_max_in_string:
 	
 not_same:
 	addi $s0, $s0, 1
-	j find_max_in_string
+	j find_min_in_string
 
 end_main:
 	li $v0, 10			
